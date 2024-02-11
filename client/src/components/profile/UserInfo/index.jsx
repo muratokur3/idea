efd1e79e7210f5564f049b768b9835be79d9648f
muratokur3 <muratokur3@icloud.com>
2024-02-11 23:49:50 +0300

    master

--- client/src/components/profile/UserInfo/index.jsx
+++ client/src/components/profile/UserInfo/index.jsx
@@ -1,18 +1,12 @@
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import LanguageIcon from "@mui/icons-material/Language";
 import XIcon from "@mui/icons-material/X";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import PinterestIcon from '@mui/icons-material/Pinterest';
 const UserInfo = () => {
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
+ 
   return (
     <Container sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <Box
@@ -26,7 +20,7 @@ const UserInfo = () => {
       >
         <Grid container spacing={2}>
           <Grid xs={12}>
-            <Typography variant="h5">John Doe</Typography>
+            <Typography variant="h5">Murat OKUR</Typography>
             <Typography variant="body3">Responsible IT Factory</Typography>
             <Typography variant="body2">01.01.1990</Typography>
           </Grid>
@@ -35,30 +29,33 @@ const UserInfo = () => {
             <Typography variant="body2">Arçelik Global</Typography>
           </Grid>
           <Grid xs={6}>
-            <Typography variant="h6">University of Helsinki</Typography>
-            <Typography variant="body2">Computer Science</Typography>
+            <Typography variant="h6">İstanbul Üniversitesi</Typography>
+            <Typography variant="body2">Computer Engineer</Typography>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={6}>
             <GitHubIcon />
             <Typography>github.com/muratokur3</Typography>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={6}>
             <LinkedInIcon />
             <Typography>linkedin/in/muratokur3</Typography>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={6}>
             <YouTubeIcon />
             <Typography>youtube.com.tr/muratokur</Typography>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={6}>
             <LanguageIcon />
             <Typography>muratokur.com.tr</Typography>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={6}>
             <XIcon />
             <Typography>twitter.com/muratokur3</Typography>
           </Grid>
-          <Grid xs={4}></Grid>
+          <Grid xs={4}>
+            <PinterestIcon />
+            <Typography>pinterest.com/muratokur3</Typography>
+          </Grid>
         </Grid>
       </Box>
     </Container>
